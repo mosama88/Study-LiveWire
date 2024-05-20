45ab1481b1e29df9b0d77e13ea954cf9cde70902
mosama88 <mosama88@hotmail.com>
2024-05-20 14:24:39 +0300

    Upload Files

--- app/Http/Livewire/FileUploudComponent.php
+++ app/Http/Livewire/FileUploudComponent.php
@@ -3,11 +3,128 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class FileUploudComponent extends Component
 {
+use WithFileUploads;
+
+
+
+    public $photo;
+
+
+    public function rules(){
+        return[
+        'photo'=> 'required|image|mimes:jpg,png,jpeg,webp',
+        ];
+
+    }
+
+
+    public function messages(){
+        return[
+            'photo.required'=> 'حقل رفع الملفات مطلوب',
+            'photo.image'=> 'يجب رفع الملفات يكون من نوع ملف ',
+            'photo.mimes'=> 'يجب رفع الملفات يكون من نوع (jpg,png,jpeg,webp) ',
+            ];
+
+    }
+
+//    public function updatedPhoto(){
+//
+//        $this->validate();
+//
+//    }
+    public function submit(){
+
+        $this->validate($this->rules(),$this->messages());
+        $this->photo->storeAs('public', 'avatar.jpg');
+
+    }
+
     public function render()
     {
         return view('livewire.file-uploud-component');
     }
+
+
+
+
+
+
+
+
+
+
+
+
+//    public $photos = [];
+//
+//
+//    public function rules(){
+//        return[
+//            'photos.*'=> 'required|image|mimes:jpg,png,jpeg,webp',
+//        ];
+//
+//    }
+//
+//
+//    public function messages(){
+//        return[
+//            'photos.*.required'=> 'حقل رفع الملفات مطلوب',
+//            'photos.*.image'=> 'يجب رفع الملفات يكون من نوع ملف ',
+//            'photos.*.mimes'=> 'يجب رفع الملفات يكون من نوع (jpg,png,jpeg,webp) ',
+//        ];
+//
+//    }
+//
+//
+//    public function submit(){
+//
+//        $this->validate($this->rules(),$this->messages());
+//
+//        foreach ($this->photos as $photo){
+//        $this->photo->storeAs('public', 'avatar.jpg');
+//        }
+//    }
+//
+//    public function render()
+//    {
+//        return view('livewire.file-uploud-component');
+//    }
+
+
+//    public $photo;
+//
+//
+//    public function rules(){
+//        return[
+//        'photo'=> 'required|image|mimes:jpg,png,jpeg,webp',
+//        ];
+//
+//    }
+//
+//
+//    public function messages(){
+//        return[
+//            'photo.required'=> 'حقل رفع الملفات مطلوب',
+//            'photo.image'=> 'يجب رفع الملفات يكون من نوع ملف ',
+//            'photo.mimes'=> 'يجب رفع الملفات يكون من نوع (jpg,png,jpeg,webp) ',
+//            ];
+//
+//    }
+//
+//
+//    public function submit(){
+//
+//        $this->validate($this->rules(),$this->messages());
+//        $this->photo->storeAs('public', 'avatar.jpg');
+//
+//    }
+//
+//    public function render()
+//    {
+//        return view('livewire.file-uploud-component');
+//    }
 }
--- resources/views/livewire/file-uploud-component.blade.php
+++ resources/views/livewire/file-uploud-component.blade.php
@@ -1,3 +1,33 @@
-<div>
-    {{-- Success is as dangerous as failure. --}}
-</div>
+
+<form action="" class="m-auto my-4 w-50" wire:submit.prevent="submit" autocomplete="off" enctype="multipart/form-data">
+    @if(session('success')!=null)
+        <div class="alert alert-success text-center">
+            {{session('success')}}
+        </div>
+    @endif
+
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Upload File</label>
+        <input type="file" class="form-control" wire:model="photo" />
+        @error('photo')
+        <div class="alert text-danger" role="alert">
+            {{$message}}
+        @enderror
+        </div>
+
+        <div class="mb-3 ">
+          <small wire:loading wire:target="photo    ">Uploading.....</small>
+        </div>
+
+
+        <div class="mb-3 my-3">
+            @if ($photo)
+                Photo Preview:
+                <img src="{{ $photo->temporaryUrl() }}">
+            @endif
+        </div>
+
+            <div class="mb-3 my-3">
+                <button type="submit" class="btn btn-outline-primary mb-3">Confirm identity</button>
+            </div>
+</form>
